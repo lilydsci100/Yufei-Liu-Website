5ed49a2a7be7115a3240121a196604407caeeedc
lilydsci100 <yfei2021@student.ubc.ca>
2025-08-29 15:13:00 -0700

    重装依赖

--- src/App.jsx
+++ src/App.jsx
@@ -2,10 +2,8 @@ import React from "react";
 import Header from "./sections/Header.jsx";
 import Hero from "./sections/Hero.jsx";
 import Work from "./sections/Work.jsx";
-import Pricing from "./sections/Pricing.jsx";
 import Faq from "./sections/Faq.jsx";
 import Connect from "./sections/Connect.jsx";
-import Download from "./sections/Download.jsx";
 import Footer from "./sections/Footer.jsx";
 import Project from "./sections/Project.jsx";
 
